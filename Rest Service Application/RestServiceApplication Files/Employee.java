657092e635a99b62b4daa3f80b5e77b713dbab73
EMMATECH25 <156687229+EMMATECH25@users.noreply.github.com>
2025-02-05 06:30:43 +0100

    Update Employee.java

--- Rest Service Application/RestServiceApplication Files/Employee.java
+++ Rest Service Application/RestServiceApplication Files/Employee.java
@@ -2,28 +2,30 @@ package com.example.restservice;
 
 public class Employee {
 
-	private final String employee_id;
-    private final String first_name;
-    private final String last_name;
-    private final String email;
-    private final String title;
+private final String employee_id;
+private final String first_name;
+private final String last_name;
+private final String email;
+private final String title;
 
-    public Employee(String employee_id,
-                    String first_name,
-                    String last_name,
-                    String email,
-                    String title)
-    {
-        this.employee_id = employee_id;
+	
+
+public Employee(String employee_id,
+                String first_name,
+                String last_name,
+                String email,
+                String title)
+{
+	this.employee_id = employee_id;
         this.first_name = first_name;
         this.last_name = last_name;
         this.email = email;
         this.title = title;
-    }
+}
 
-    @Override
-    public String toString()
-    {
+@Override
+public String toString()
+{
   
         return "Employee: ID = "
             + employee_id + ", First Name = "
@@ -31,31 +33,31 @@ public class Employee {
             + last_name + ", email = "
             + email + ", title = "
             + title;      
-    }
+}
 
-    public String getId()
-    {
+public String getId()
+{
         return employee_id;
-    }
+}
 
-    public String getFirstName()
-    {
+public String getFirstName()
+{
         return first_name;
-    }
+}
 
-    public String getLastName()
-    {
+public String getLastName()
+{
         return last_name;
-    }
+}
 
-    public String getEmail()
-    {
+public String getEmail()
+{
         return email;
-    }
+}
 
-    public String getTitle()
-    {
+public String getTitle()
+{
         return title;
-    }
+}
 
 }
